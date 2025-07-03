d43f2452c66af9609666f8ad98dd5493b58f644e
C.V.Malavika <cvmalavika@gmail.com>
2025-07-03 10:44:25 +0530

    first commit

---
+++ Neutrosophic_Cubic_Sets.py
@@ -0,0 +1,431 @@
+class INS:
+
+    def __init__(self,
+                 T_lower, T_upper,
+                 I_lower, I_upper,
+                 F_lower, F_upper):
+
+        assert 0 <= T_lower <= 1
+        assert 0 <= T_upper <= 1
+        assert 0 <= I_lower <= 1
+        assert 0 <= I_upper <= 1
+        assert 0 <= F_lower <= 1
+        assert 0 <= F_upper <= 1
+        assert T_lower <= T_upper
+        assert I_lower <= I_upper
+        assert F_lower <= F_upper
+        assert 0 <= T_lower + I_lower + F_lower <= 3
+        assert 0 <= T_upper + I_upper + F_upper <= 3
+
+        self.T_lower = T_lower
+        self.T_upper = T_upper
+        self.I_lower = I_lower
+        self.I_upper = I_upper
+        self.F_lower = F_lower
+        self.F_upper = F_upper
+
+
+    def __str__(self):
+
+        return (f"<[{self.T_lower}, {self.T_upper}],"
+                f"[{self.I_lower},{self.I_upper}],"
+                f"[{self.F_lower},{self.F_upper}]>")
+    
+
+class NS:
+
+    def __init__(self,
+                 truth,
+                 indeterminacy,
+                 falsehood):
+
+        assert 0 <= truth <= 1
+        assert 0 <= indeterminacy <= 1
+        assert 0 <= falsehood <= 1
+        assert 0 <= truth + indeterminacy + falsehood <= 3
+
+        self.T = truth
+        self.I = indeterminacy
+        self.F = falsehood
+
+
+    def __str__(self):
+
+        return (f"<{self.T},"
+                f"{self.I},"
+                f"{self.F}>")
+    
+
+class NCN:
+
+    def __init__(self, T_lower, T_upper, I_lower, I_upper, F_lower, F_upper, truth, indeterminacy, falsehood):
+
+        self.ins = INS(T_lower, T_upper, I_lower, I_upper, F_lower, F_upper)
+        self.ns = NS(truth, indeterminacy, falsehood)
+
+    def __len__(self):
+
+        return len(self._number)
+
+
+    def __getitem__(self, index):
+
+        return self._number[index]
+
+
+    def __str__(self):
+
+        return (f"({str(self.ins)}, "
+               f"{str(self.ns)})")
+
+
+    def p_union(self, other):
+
+        return NCN(
+            max(self.ins.T_lower, other.ins.T_lower),
+            max(self.ins.T_upper, other.ins.T_upper),
+            max(self.ins.I_lower, other.ins.I_lower),
+            max(self.ins.I_upper, other.ins.I_upper),
+            max(self.ins.F_lower, other.ins.F_lower),
+            max(self.ins.F_upper, other.ins.F_upper),
+            max(self.ns.T, other.ns.T),
+            max(self.ns.I, other.ns.I),
+            max(self.ns.F, other.ns.F)
+        )
+
+
+    def p_intersection(self, other):
+
+        return NCN(
+            min(self.ins.T_lower, other.ins.T_lower),
+            min(self.ins.T_upper, other.ins.T_upper),
+            min(self.ins.I_lower, other.ins.I_lower),
+            min(self.ins.I_upper, other.ins.I_upper),
+            min(self.ins.F_lower, other.ins.F_lower),
+            min(self.ins.F_upper, other.ins.F_upper),
+            min(self.ns.T, other.ns.T),
+            min(self.ns.I, other.ns.I),
+            min(self.ns.F, other.ns.F)
+        )
+
+
+    def r_union(self, other):
+
+        return NCN(
+            max(self.ins.T_lower, other.ins.T_lower),
+            max(self.ins.T_upper, other.ins.T_upper),
+            max(self.ins.I_lower, other.ins.I_lower),
+            max(self.ins.I_upper, other.ins.I_upper),
+            max(self.ins.F_lower, other.ins.F_lower),
+            max(self.ins.F_upper, other.ins.F_upper),
+            min(self.ns.T, other.ns.T),
+            min(self.ns.I, other.ns.I),
+            min(self.ns.F, other.ns.F)
+        )
+
+
+    def r_intersection(self, other):
+
+        return NCN(
+            min(self.ins.T_lower, other.ins.T_lower),
+            min(self.ins.T_upper, other.ins.T_upper),
+            min(self.ins.I_lower, other.ins.I_lower),
+            min(self.ins.I_upper, other.ins.I_upper),
+            min(self.ins.F_lower, other.ins.F_lower),
+            min(self.ins.F_upper, other.ins.F_upper),
+            max(self.ns.T, other.ns.T),
+            max(self.ns.I, other.ns.I),
+            max(self.ns.F, other.ns.F)
+        )
+
+
+    def complement(self):
+
+        return NCN(
+            round(1 - self.ins.T_upper, 2),
+            round(1 - self.ins.T_lower, 2),
+            round(1 - self.ins.I_upper, 2),
+            round(1 - self.ins.I_lower, 2),
+            round(1 - self.ins.F_upper, 2),
+            round(1 - self.ins.F_lower, 2),
+            round(1 - self.ns.T, 2),
+            round(1 - self.ns.I, 2),
+            round(1 - self.ns.F, 2)
+        )
+
+
+    def __eq__(self, other):
+
+        return (
+            self.ins.T_lower == other.ins.T_lower and
+            self.ins.T_upper == other.ins.T_upper and
+            self.ins.I_lower == other.ins.I_lower and
+            self.ins.I_upper == other.ins.I_upper and
+            self.ins.F_lower == other.ins.F_lower and
+            self.ins.F_upper == other.ins.F_upper and
+            self.ns.T == other.ns.T and
+            self.ns.I == other.ns.I and
+            self.ns.F == other.ns.F
+        )
+
+
+    def __add__(self, other):
+
+        return NCN(
+              round(self.ins.T_lower + other.ins.T_lower - self.ins.T_lower * other.ins.T_lower, 2),
+              round(self.ins.T_upper + other.ins.T_upper - self.ins.T_upper * other.ins.T_upper, 2),
+              round(self.ins.I_lower * other.ins.I_lower, 2),
+              round(self.ins.I_upper * other.ins.I_upper, 2),
+              round(self.ins.F_lower * other.ins.F_lower, 2),
+              round(self.ins.F_upper * other.ins.F_upper, 2),
+              round(self.ns.T + other.ns.T - self.ns.T * other.ns.T, 2),
+              round(self.ns.I * other.ns.I, 2),
+              round(self.ns.F * other.ns.F, 2)
+        )
+
+
+    def __mul__(self, other):
+
+        return NCN(
+              round(self.ins.T_lower * other.ins.T_lower, 2),
+              round(self.ins.T_upper * other.ins.T_upper, 2),
+              round(self.ins.I_lower + other.ins.I_lower - self.ins.I_lower * other.ins.I_lower, 2),
+              round(self.ins.I_upper + other.ins.I_upper - self.ins.I_upper * other.ins.I_upper, 2),
+              round(self.ins.F_lower + other.ins.F_lower - self.ins.F_lower * other.ins.F_lower, 2),
+              round(self.ins.F_upper + other.ins.F_upper - self.ins.F_upper * other.ins.F_upper, 2),
+              round(self.ns.T * other.ns.T, 2),
+              round(self.ns.I + other.ns.I - self.ns.I * other.ns.I, 2),
+              round(self.ns.F + other.ns.F - self.ns.F * other.ns.F, 2)
+        )
+
+
+    def scalar_multiplication(self, scalar):
+
+        assert scalar > 0
+
+        return NCN(
+              round(1 - ((1 - self.ins.T_lower) ** scalar), 2),
+              round(1 - ((1 - self.ins.T_upper) ** scalar), 2),
+              round(self.ins.I_lower ** scalar, 2),
+              round(self.ins.I_upper ** scalar, 2),
+              round(self.ins.F_lower ** scalar, 2),
+              round(self.ins.F_upper ** scalar, 2),
+              round(1 - ((1 - self.ns.T) ** scalar), 2),
+              round(self.ns.I ** scalar, 2),
+              round(self.ns.F ** scalar, 2)
+        )
+
+
+    def containment(self, other):
+
+        return (
+            self.ins.T_lower < other.ins.T_lower and
+            self.ins.T_upper < other.ins.T_upper and
+            self.ins.I_lower > other.ins.I_lower and
+            self.ins.I_upper > other.ins.I_upper and
+            self.ins.F_lower > other.ins.F_lower and
+            self.ins.F_upper > other.ins.F_upper and
+            self.ns.T < other.ns.T and
+            self.ns.I > other.ns.I and
+            self.ns.F > other.ns.F
+        )
+
+
+    def score(self):
+
+        return (
+            round(((6 + self.ins.T_lower + self.ins.T_upper
+          - self.ins.I_lower - self.ins.I_upper
+          - self.ins.F_lower - self.ins.F_upper
+          + self.ns.T - self.ns.I - self.ns.F) / 9), 2)
+        )
+
+
+    def accuracy(self):
+
+        return (
+            round((self.ins.T_lower + self.ins.T_upper
+          - self.ins.F_lower - self.ins.F_upper
+          + self.ns.T - self.ns.F), 2)
+        )
+
+
+    def certainty(self):
+
+        return (
+            round((self.ins.T_lower + self.ins.T_upper + self.ns.T), 2)
+        )
+
+
+    def distance_measure(self, other):
+
+      return (1 / 9) * (
+          abs(self.ins.T_lower - other.ins.T_lower) +
+          abs(self.ins.I_lower - other.ins.I_lower) +
+          abs(self.ins.F_lower - other.ins.F_lower) +
+          abs(self.ins.T_upper - other.ins.T_upper) +
+          abs(self.ins.I_upper - other.ins.I_upper) +
+          abs(self.ins.F_upper - other.ins.F_upper) +
+          abs(self.ns.T - other.ns.T) +
+          abs(self.ns.I - other.ns.I) +
+          abs(self.ns.F - other.ns.F)
+      )
+    
+
+class NCS:
+
+    def __init__(self):
+
+        self.ncs = []
+
+
+    def add_element(self, ncn):
+
+        assert isinstance(ncn, NCN)
+        self.ncs.append(ncn)
+
+
+    def __str__(self):
+
+        string = "{\n"
+        for i in range(len(self.ncs)):
+            if i < len(self.ncs) - 1:
+                string += f"{self.ncs[i]}, \n"
+            else:
+                string += f"{self.ncs[i]}"
+        string += "\n}"
+
+        return string
+
+
+    def __len__(self):
+
+        return len(self.ncs)
+
+
+    def __getitem__(self, index):
+
+        return self.ncs[index]
+
+
+    def p_union(self, other):
+
+        p_union_set = NCS()
+        for i in range(len(self.ncs)):
+            p_union_set.add_element(self.ncs[i].p_union(other.ncs[i]))
+
+        return p_union_set
+
+
+    def p_intersection(self, other):
+
+        p_intersection_set = NCS()
+        for i in range(len(self.ncs)):
+            p_intersection_set.add_element(self.ncs[i].p_intersection(other.ncs[i]))
+
+        return p_intersection_set
+
+
+    def r_union(self, other):
+
+        r_union_set = NCS()
+        for i in range(len(self.ncs)):
+            r_union_set.add_element(self.ncs[i].r_union(other.ncs[i]))
+
+        return r_union_set
+
+
+    def r_intersection(self, other):
+
+        r_intersection_set = NCS()
+        for i in range(len(self.ncs)):
+            r_intersection_set.add_element(self.ncs[i].r_intersection(other.ncs[i]))
+
+        return r_intersection_set
+
+
+    def complement(self):
+
+        complement_set = NCS()
+        for i in range(len(self.ncs)):
+            complement_set.add_element(self.ncs[i].complement())
+
+        return complement_set
+
+    def __eq__(self, other):
+
+        for i in range(len(self.ncs)):
+            if not (self.ncs[i] == other.ncs[i]):
+                return False
+        return True
+
+
+    def __add__(self, other):
+
+        add_set = NCS()
+        for i in range(len(self.ncs)):
+            add_set.add_element(self.ncs[i] + other.ncs[i])
+
+        return add_set
+
+
+    def __mul__(self, other):
+
+        mul_set = NCS()
+        for i in range(len(self.ncs)):
+            mul_set.add_element(self.ncs[i] * other.ncs[i])
+
+        return mul_set
+
+
+    def scalar_multiplication(self, scalar):
+
+        scalar_mul_set = NCS()
+        for i in range(len(self.ncs)):
+            scalar_mul_set.add_element(self.ncs[i].scalar_multiplication(scalar))
+
+        return scalar_mul_set
+
+
+    def containment(self, other):
+
+        for i in range(len(self.ncs)):
+            if not(self.ncs[i].containment(other.ncs[i])):
+                return False
+        return True
+
+
+    def score(self):
+
+        score_set = []
+        for i in range(len(self.ncs)):
+            score_set.append(self.ncs[i].score())
+
+        return score_set
+
+
+    def accuracy(self):
+
+        accuracy_set = []
+        for i in range(len(self.ncs)):
+            accuracy_set.append(self.ncs[i].accuracy())
+
+        return accuracy_set
+
+
+    def certainty(self):
+
+        certainty_set = []
+        for i in range(len(self.ncs)):
+            certainty_set.append(self.ncs[i].certainty())
+
+        return certainty_set
+
+
+    def distance_measure(self, other):
+
+        distance_set = []
+        for i in range(len(self.ncs)):
+            distance_set.append(self.ncs[i].distance_measure(other.ncs[i]))
+
+        return (1 / len(self._item)) * sum(distance_set)
---
+++ app.py
@@ -0,0 +1,260 @@
+import streamlit as st
+import time
+from Neutrosophic_Cubic_Sets import NCS, NCN
+
+# Page setup
+st.set_page_config(page_title = "NCS Tool", layout = "wide")
+
+# Custom CSS to hide all link icons
+
+st.markdown("""
+    <style>
+        /* Hide all link icons and prevent link behavior */
+        a {
+            text-decoration: none !important;
+            pointer-events: none !important;
+            color: inherit !important;
+        }
+
+        /* Hide fullscreen buttons */
+        button[title = "View fullscreen"] {
+            display: none !important;
+        }
+
+        /* Hide markdown link icons */
+        [data-testid = "stMarkdownContainer"] a {
+            display: none !important;
+        }
+
+        /* Textarea styling */
+        .stTextArea textarea {
+            font-family: monospace;
+        }
+
+        /* Control page width */
+        .stMainBlockContainer {
+            max-width: 80rem;
+        }
+    </style>
+    """, unsafe_allow_html = True)
+
+st.markdown("<h1 style = 'text-align: center;'>Python Tool for Neutrosophic Cubic Sets</h1>", unsafe_allow_html = True)
+
+
+# Session State Initialization
+for key in ["ncs_list_A", "ncs_list_B", "message_A", "message_B"]:
+    if key not in st.session_state:
+        st.session_state[key] = [] if "list" in key else ""
+
+
+# --- Input Columns for A and B ---
+colA, colB = st.columns(2)
+
+with colA:
+    st.markdown("<h3 style = 'text-align: center;'>NCS A</h3>", unsafe_allow_html = True)
+    T_A = tuple(round(v, 2) for v in st.slider("Lower Truth, Upper Truth", 0.0, 1.0, (0.2, 0.3), 0.01, key = "T_A"))
+    I_A = tuple(round(v, 2) for v in st.slider("Lower Indeterminacy, Upper Indeterminacy", 0.0, 1.0, (0.3, 0.5), 0.01, key = "I_A"))
+    F_A = tuple(round(v, 2) for v in st.slider("Lower Fallacy, Upper Fallacy", 0.0, 1.0, (0.7, 0.9), 0.01, key = "F_A"))
+    t_A = round(st.number_input("Truth", 0.0, 1.0, 0.35, 0.01, key = "t_A"), 2)
+    i_A = round(st.number_input("Indeterminacy", 0.0, 1.0, 0.25, 0.01, key = "i_A"), 2)
+    f_A = round(st.number_input("Fallacy", 0.0, 1.0, 0.4, 0.01, key = "f_A"), 2)
+
+    if st.button("Add this element to A"):
+        if len(st.session_state.ncs_list_A) > len(st.session_state.ncs_list_B):
+            st.warning("Add an element to B before adding more to A.")
+        else:
+            st.session_state.ncs_list_A.append([list(T_A), list(I_A), list(F_A), [t_A, i_A, f_A]])
+            st.session_state.message_A = "Added element to NCS A."
+
+    if st.session_state.message_A:
+        st.success(st.session_state.message_A)
+        time.sleep(0.5)
+        st.session_state.message_A = ""
+        st.rerun()
+
+    if st.session_state.ncs_list_A:
+        try:
+            NCS_A = NCS()
+            for item in st.session_state.ncs_list_A:
+                NCS_A.add_element(NCN(*item[0], *item[1], *item[2], *item[3]))
+            st.session_state.NCS_A_obj = NCS_A
+            st.text_area("NCS A be displayed here...", value = str(NCS_A), height = 150)
+        except Exception as e:
+            st.error(f"Error creating NCS A: {e}")
+
+    if st.button("Reset A"):
+        st.session_state.ncs_list_A = []
+        st.rerun()
+
+with colB:
+    st.markdown("<h3 style = 'text-align: center;'>NCS B</h3>", unsafe_allow_html = True)
+    T_B = tuple(round(v, 2) for v in st.slider("Lower Truth, Upper Truth", 0.0, 1.0, (0.2, 0.3), 0.01, key = "T_B"))
+    I_B = tuple(round(v, 2) for v in st.slider("Lower Indeterminacy, Upper Indeterminacy", 0.0, 1.0, (0.3, 0.5), 0.01, key = "I_B"))
+    F_B = tuple(round(v, 2) for v in st.slider("Lower Fallacy, Upper Fallacy", 0.0, 1.0, (0.7, 0.9), 0.01, key = "F_B"))
+    t_B = round(st.number_input("Truth", 0.0, 1.0, 0.35, 0.01, key = "t_B"), 2)
+    i_B = round(st.number_input("Indeterminacy", 0.0, 1.0, 0.25, 0.01, key = "i_B"), 2)
+    f_B = round(st.number_input("Fallacy", 0.0, 1.0, 0.4, 0.01, key = "f_B"), 2)
+
+    if st.button("Add this element to B"):
+        if len(st.session_state.ncs_list_B) > len(st.session_state.ncs_list_A):
+            st.warning("Add an element to A before adding more to B.")
+        else:
+            st.session_state.ncs_list_B.append([list(T_B), list(I_B), list(F_B), [t_B, i_B, f_B]])
+            st.session_state.message_B = "Added element to NCS B."
+
+    if st.session_state.message_B:
+        st.success(st.session_state.message_B)
+        time.sleep(0.5)
+        st.session_state.message_B = ""
+        st.rerun()
+
+    if st.session_state.ncs_list_B:
+        try:
+            NCS_B = NCS()
+            for item in st.session_state.ncs_list_B:
+                NCS_B.add_element(NCN(*item[0], *item[1], *item[2], *item[3]))
+            st.session_state.NCS_B_obj = NCS_B
+            st.text_area("NCS B be displayed here...", value = str(NCS_B), height = 150)
+        except Exception as e:
+            st.error(f"Error creating NCS B: {e}")
+
+    if st.button("Reset B"):
+        st.session_state.ncs_list_B = []
+        st.rerun()
+
+# NCS Operations
+
+if not st.session_state["ncs_list_A"] and not st.session_state["ncs_list_B"]:
+    st.warning("Please add at least one element to both NCS A and NCS B to continue.")
+
+elif not st.session_state["ncs_list_A"]:
+    st.warning("Please add at least one element to NCS A to continue.")
+
+elif not st.session_state["ncs_list_B"]:
+    st.warning("Please add at least one element to NCS B to continue.")
+
+elif len(st.session_state["ncs_list_A"]) < len(st.session_state["ncs_list_B"]):
+    st.warning("NCS A has fewer elements. Please add another element to NCS A.")
+
+elif len(st.session_state["ncs_list_A"]) > len(st.session_state["ncs_list_B"]):
+    st.warning("NCS B has fewer elements. Please add another element to NCS B.")
+
+else:
+
+    st.markdown("---")
+    st.subheader("Operations on Neutrosophic Cubic Sets")
+    result = ""
+
+    # Unary operations
+    c1, c2 = st.columns(2)
+    st.markdown("<h5>Unary Operations</h5>", unsafe_allow_html = True)
+    with c1:
+        unary_target = st.radio("Select NCS for unary operations:", ["NCS A", "NCS B"], horizontal = True)
+    with c2:
+        scalar = st.number_input("Enter scalar (For Scalar Multiplication):", min_value = 0.0, value = 1.0, step = 0.1, key = "scalar_input")
+
+    c1, c2, c3, c4, c5 = st.columns(5)
+
+    with c1:
+        if st.button("COMPLEMENT"):
+            try:
+                obj = st.session_state.get(f"NCS_{unary_target[-1]}_obj")
+                result = str(obj.complement())
+            except Exception as e:
+                st.error(f"Error in complement: {e}")
+
+    with c2:
+        if st.button("ACCURACY"):
+            try:
+                obj = st.session_state.get(f"NCS_{unary_target[-1]}_obj")
+                result = str(obj.accuracy())
+            except Exception as e:
+                st.error(f"Error in accuracy: {e}")
+
+    with c3:
+        if st.button("SCORE"):
+            try:
+                obj = st.session_state.get(f"NCS_{unary_target[-1]}_obj")
+                result = str(obj.score())
+            except Exception as e:
+                st.error(f"Error in score: {e}")
+
+    with c4:
+        if st.button("CERTAINTY"):
+            try:
+                obj = st.session_state.get(f"NCS_{unary_target[-1]}_obj")
+                result = str(obj.certainty())
+            except Exception as e:
+                st.error(f"Error in certainty: {e}")
+
+    with c5:
+        if st.button("SCALAR MULTIPLICATION"):
+            try:
+                obj = st.session_state.get(f"NCS_{unary_target[-1]}_obj")
+                result = str(obj.scalar_multiplication(scalar))
+            except Exception as e:
+                st.error(f"Error in scalar multiplication: {e}")
+
+    # Binary operations
+    st.markdown("<h5>Binary Operations</h5>", unsafe_allow_html = True)
+    c1, c2, c3, c4 = st.columns(4)
+    with c1:
+        if st.button("P-UNION"):
+            try:
+                result = str(st.session_state.NCS_A_obj.p_union(st.session_state.NCS_B_obj))
+            except Exception as e:
+                st.error(f"Error in P-Union: {e}")
+
+    with c2:
+        if st.button("P-INTERSECTION"):
+            try:
+                result = str(st.session_state.NCS_A_obj.p_intersection(st.session_state.NCS_B_obj))
+            except Exception as e:
+                st.error(f"Error in P-Intersection: {e}")
+
+    with c3:
+        if st.button("R-UNION"):
+            try:
+                result = str(st.session_state.NCS_A_obj.r_union(st.session_state.NCS_B_obj))
+            except Exception as e:
+                st.error(f"Error in R-Union: {e}")
+
+    with c4:
+        if st.button("R-INTERSECTION"):
+            try:
+                result = str(st.session_state.NCS_A_obj.r_intersection(st.session_state.NCS_B_obj))
+            except Exception as e:
+                st.error(f"Error in R-Intersection: {e}")
+
+    c1, c2, c3, c4 = st.columns(4)
+    with c1:
+        if st.button("EQUALITY"):
+            try:
+                result = st.session_state.NCS_A_obj = st.session_state.NCS_B_obj
+            except Exception as e:
+                st.error(f"Error in equality: {e}")
+
+    with c2:
+        if st.button("ADDITION"):
+            try:
+                result = str(st.session_state.NCS_A_obj + st.session_state.NCS_B_obj)
+            except Exception as e:
+                st.error(f"Error in addition: {e}")
+
+    with c3:
+        if st.button("MULTIPLICATION"):
+            try:
+                result = str(st.session_state.NCS_A_obj * st.session_state.NCS_B_obj)
+            except Exception as e:
+                st.error(f"Error in multiplication: {e}")
+
+    with c4:
+        if st.button("CONTAINMENT"):
+            try:
+                result = st.session_state.NCS_A_obj.containment(st.session_state.NCS_B_obj)
+            except Exception as e:
+                st.error(f"Error in containment: {e}")
+
+    # Result
+    st.markdown("<h4>Result of the Operation</h4>", unsafe_allow_html = True)
+    st.text_area("Result will be displayed here...", value = str(result), height = 150)
